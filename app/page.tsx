e40068cadab8b539585c58678a74a9a936cd2c14
VelizarDabov <velizardabov@gmail.com>
2023-11-14 12:00:38 +0200

    starting project

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Header from '@/components/Header'
+
+export default function Home() {
+  return (
+    <main>
+      {/*header*/}
+<Header/>
+       {/*bord*/}
+     <h1>Trello</h1>
+    </main>
+  )
+}
