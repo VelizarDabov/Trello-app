09e50a02c45b0aec16d398497f6fbf3c421180ce
Velizar Dabov <velizardabov@gmail.com>
2024-02-04 21:31:56 +0200

    types data

--- components/TaskTypeRadioGroup.tsx
+++ components/TaskTypeRadioGroup.tsx
@@ -2,27 +2,27 @@
 import { RadioGroup } from '@headlessui/react'
 import { useBoardStore } from "@/store/BoardStore";
 import { CheckCircleIcon } from '@heroicons/react/20/solid';
-
-const types = [
-  {
-    id: "todo",
-    name: "Todo",
-    description: "A new task to be completed",
-    color: "bg-red-500",
-  },
-  {
-    id: "inprogres",
-    name: "In Progress",
-    description: "A task that is currently being bworkd on",
-    color: "bg-yellow-500",
-  },
-  {
-    id: "done",
-    name: "Done",
-    description: "A task that has been completed",
-    color: "bg-green-500",
-  },
-];
+import {types} from '../components/types'
+// const types = [
+//   {
+//     id: "todo",
+//     name: "Todo",
+//     description: "A new task to be completed",
+//     color: "bg-red-500",
+//   },
+//   {
+//     id: "inprogres",
+//     name: "In Progress",
+//     description: "A task that is currently being bworkd on",
+//     color: "bg-yellow-500",
+//   },
+//   {
+//     id: "done",
+//     name: "Done",
+//     description: "A task that has been completed",
+//     color: "bg-green-500",
+//   },
+// ];
 
 const TaskTypeRadioGroup = () => {
     const [setNewTaskType, newTaskType]= useBoardStore((state) => [
---
+++ components/types.tsx
@@ -0,0 +1,20 @@
+export const types = [
+      {
+        id: "todo",
+        name: "Todo",
+        description: "A new task to be completed",
+        color: "bg-red-500",
+      },
+      {
+        id: "inprogres",
+        name: "In Progress",
+        description: "A task that is currently being bworkd on",
+        color: "bg-yellow-500",
+      },
+      {
+        id: "done",
+        name: "Done",
+        description: "A task that has been completed",
+        color: "bg-green-500",
+      },
+    ];
